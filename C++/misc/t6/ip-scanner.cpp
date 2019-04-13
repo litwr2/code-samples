88f9ab2f0f177c74f69650c2824a8f1f486e6c17
micro <litwr@yandex.ru>
2019-04-13 17:00:02 +0300

    ip-scanner v3

--- C++/misc/t6/ip-scanner.cpp
+++ C++/misc/t6/ip-scanner.cpp
@@ -25,7 +25,7 @@ Given 10,000 ip-addresses of servers. It is necessary to poll addresses on avail
 
 #define NUMBER_OF_THREADS 200
 #define DELAY1_MS 1
-#define DELAY2_MS 30
+#define DELAY2_MS 10
 
 // Get current date/time, format is dd-MM-yyyy hh:mm:ss.fff
 const std::string currentDateTime(const std::chrono::milliseconds& ms) {
@@ -95,8 +95,7 @@ int main(int argc, char *argv[]) {
 
     std::atomic<int> qt(0);
     std::mutex mx, mxw;
-    std::map<std::pair<std::string, int>, std::chrono::milliseconds> serverSet, serverSetUpdate;
-    std::set<std::pair<std::string, int>> waiting;
+    std::set<std::pair<std::string, int>> waiting, serverSet;
     for (auto v: servers) {
         ++qt;
         std::thread([&, v]{
@@ -105,7 +104,7 @@ int main(int argc, char *argv[]) {
             mxw.unlock();
             if (checkServer(v.first,  v.second) == 0) {
                 std::lock_guard<std::mutex> lk(mx);
-                serverSet.insert({v, std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())});
+                serverSet.insert(v);
             }
             mxw.lock();
             waiting.erase(v);
@@ -114,42 +113,42 @@ int main(int argc, char *argv[]) {
         std::this_thread::sleep_for(std::chrono::milliseconds(DELAY1_MS));
         while (qt >= NUMBER_OF_THREADS);
     }
-    std::multimap<std::chrono::milliseconds, std::string> sortedOutput;
     std::this_thread::sleep_for(std::chrono::milliseconds(DELAY2_MS));
 ETERNAL_LOOP:
-    sortedOutput.clear();
     for (auto v: servers) {
         {
-        std::lock_guard<std::mutex> lk(mxw);
-        if (waiting.find(v) != waiting.end()) continue;
+            std::lock_guard<std::mutex> lk(mxw);
+            if (waiting.find(v) != waiting.end())
+                continue;
         }
         ++qt;
         std::thread([&, v]{
-            std::unique_lock<std::mutex> lk(mxw);
+            std::unique_lock<std::mutex> lkw(mxw);
             waiting.insert(v);
-            lk.unlock();
-            if (checkServer(v.first,  v.second) == 0) {
-                std::lock_guard<std::mutex> lk(mx);
-                serverSetUpdate.insert({v, std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())});
+            lkw.unlock();
+            int r = checkServer(v.first,  v.second);
+            auto t = currentDateTime(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())) + ", " + v.first + ":" + std::to_string(v.second);
+            mx.lock();
+            if (r == 0) {
+                if (serverSet.find(v) == serverSet.end()) {
+                     std::cout << t + " up\n";
+                     serverSet.insert(v);
+                }
             }
-            lk.lock();
+            else {
+                if (serverSet.find(v) != serverSet.end()) {
+                     std::cout << t + " down\n";
+                     serverSet.erase(v);
+                }
+            }
+            mx.unlock();
+            lkw.lock();
             waiting.erase(v);
             --qt;}).detach();
         std::this_thread::sleep_for(std::chrono::milliseconds(DELAY1_MS));
         while (qt >= NUMBER_OF_THREADS);
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(DELAY2_MS));
-    mx.lock();
-    for (auto el: serverSet)
-        if (serverSetUpdate.find(el.first) == serverSetUpdate.end())
-            sortedOutput.insert({el.second, currentDateTime(el.second) + ", " + el.first.first + ":" + std::to_string(el.first.second) + " down\n"});
-    for (auto el: serverSetUpdate)
-        if (serverSet.find(el.first) == serverSet.end())
-            sortedOutput.insert({el.second, currentDateTime(el.second) + ", " + el.first.first + ":" + std::to_string(el.first.second) + " up\n"});
-    serverSet = std::move(serverSetUpdate);
-    mx.unlock();
-    for (auto el: sortedOutput)
-        outputAll(output, el.second);
     goto ETERNAL_LOOP;
     output.close();
     return 0;
